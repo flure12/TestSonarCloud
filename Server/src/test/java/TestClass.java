cf4a20e46005c35cd475f25d8364ccaddbebee20
Florian Maurer <flmaurer@icloud.com>
2020-05-18 20:19:57 +0200

    Heheh finally a sucess

--- Server/src/test/java/TestClass.java
+++ Server/src/test/java/TestClass.java
@@ -1,8 +1,11 @@
+import com.example.server.Server;
+
 import org.junit.Test;
 
 public class TestClass {
 @org.junit.Test
 public void test () {
-
+    Server sc = new Server();
+    sc.testMethd();
 }
 }
