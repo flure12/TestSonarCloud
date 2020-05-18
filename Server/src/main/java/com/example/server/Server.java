59fe2b9c91fa0fbe3cd6058e4405a9183c617f74
Florian Maurer <flmaurer@icloud.com>
2020-05-18 19:55:18 +0200

    Nobody give me this time back but i added an test

--- Server/src/main/java/com/example/server/Server.java
+++ Server/src/main/java/com/example/server/Server.java
@@ -1,7 +1,7 @@
 package com.example.server;
 
 public class Server {
-    public static void main(String[] args) {
-        System.out.print("Hello World");
-    }
+         public void testMethd () {
+             System.out.print("Hello World");
+         }
 }
